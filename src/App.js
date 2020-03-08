ceff486a32bb4b4f78fb54a986d071bbed3f1042
maxymus-docsify <max@docsify.co>
2020-03-08 11:38:43 +0000

    Implemented navigation between from the main screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // eslint-disable
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 
-import {LetterGame, WordGame} from './components';
+
+import {LetterGame, WordGame, Word} from './components';
 
 
 function App() {
@@ -12,7 +14,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <WordGame />
+
+        <BrowserRouter>
+          <Switch>
+            <Route path='/' exact  >
+              <Link to="/words" style={{color: 'white'}}>Word Game</Link>
+              <Link to="/letters" style={{color: 'white'}}>Letters Game</Link>
+            </Route>
+            <Route component={WordGame} path="/words" />
+            <Route component={LetterGame} path="/letters" />
+          </Switch>
+          
+        </BrowserRouter>
+
+     
       </header>
     </div>
   );
