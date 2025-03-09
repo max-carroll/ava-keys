7ef6e069b96aace63573c9ef86dbb66a7816054b
Max Carroll <maxymus@gmail.com>
2025-03-09 15:08:20 +0000

    vite - fixing linting errors/ typing errors

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -1,9 +1,13 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
 
-export default function useEventListener(eventName, handler, element = window){
+export default function useEventListener(
+  eventName: string,
+  handler: (event: Event) => void,
+  element = window
+) {
   // Create a ref that stores handler
-  const savedHandler = useRef();
-  
+  const savedHandler = useRef<(event: Event) => void>(null);
+
   // Update ref.current value if handler changes.
   // This allows our effect below to always get latest handler ...
   // ... without us needing to pass it in effect deps array ...
@@ -15,16 +19,16 @@ export default function useEventListener(eventName, handler, element = window){
   useEffect(
     () => {
       // Make sure element supports addEventListener
-      // On 
+      // On
       const isSupported = element && element.addEventListener;
       if (!isSupported) return;
-      
+
       // Create event listener that calls handler function stored in ref
-      const eventListener = event => savedHandler.current(event);
-      
+      const eventListener = (event: Event) => savedHandler.current?.(event);
+
       // Add event listener
       element.addEventListener(eventName, eventListener);
-      
+
       // Remove event listener on cleanup
       return () => {
         element.removeEventListener(eventName, eventListener);
@@ -32,4 +36,4 @@ export default function useEventListener(eventName, handler, element = window){
     },
     [eventName, element] // Re-run if eventName or element changes
   );
-};
+}
