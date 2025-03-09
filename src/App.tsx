69c669d7c12eed077c77e50355e9f487985c9c30
Max Carroll <maxymus@gmail.com>
2025-03-09 14:43:38 +0000

    vite -converting app.jsx to tsx and upgrading react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// eslint-disable
+import './App.css';
+import { BrowserRouter,  Route, Link, Routes } from 'react-router'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+
+
+import { LetterGame, WordGame, NumberGame, Tagalog } from './games';
+import { Speak } from './games/Speak';
+import { SaveWords } from './components/SaveWords';
+import { Flappy } from './components/Flappy';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <AppBar>
+          <Toolbar>
+            <Typography><Link to="/words" style={{ color: 'white', textDecoration: 'none' }}><Button>Word Game</Button></Link></Typography>
+            <Typography><Link to="/numbers" style={{ color: 'white', textDecoration: 'none' }}><Button>NumberGame</Button></Link></Typography>
+            <Typography><Link to="/letters" style={{ color: 'white', textDecoration: 'none'  }}><Button>Letters Game</Button></Link></Typography>
+            <Typography><Link to="/tagalog" style={{ color: 'white', textDecoration: 'none'  }}><Button>Tagalog Game</Button></Link></Typography>
+            <Typography><Link to="/talk" style={{ color: 'white', textDecoration: 'none'  }}><Button>Talk Game</Button></Link></Typography>
+            <Typography><Link to="/savewords" style={{ color: 'white', textDecoration: 'none'  }}><Button>Save Words</Button></Link></Typography>
+            <Typography><Link to="/flappy" style={{ color: 'white', textDecoration: 'none'  }}><Button>Flappy</Button></Link></Typography>
+          </Toolbar>
+        </AppBar>
+        <div className="App">
+          <header className="App-header">
+
+            <Routes>
+              <Route element={<WordGame />} path="/words" />
+              <Route element={<LetterGame />} path="/letters" />
+              <Route element={<NumberGame />} path="/numbers" />
+              <Route element={<Tagalog />} path="/tagalog" />
+              <Route element={<Speak />} path="/talk" />
+              <Route element={<SaveWords />} path="/savewords" />
+              <Route element={<Flappy />} path="/flappy" />
+            </Routes>
+
+
+
+          </header>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
